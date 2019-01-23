f3946cd0d0b8e77c3010f2245aafdbfc6383d84c
Peter Schmidt-Kallesoe <pkallesoe@gmail.com>
2019-01-23 07:16:14 +0100

    Change cd db to contain actual information instead of just dummy

--- src/app/in-memory-cd-db.service.ts
+++ src/app/in-memory-cd-db.service.ts
@@ -3,10 +3,10 @@ import { InMemoryDbService } from 'angular-in-memory-web-api';
 export class InMemoryCDDBService implements InMemoryDbService {
   createDb() {
     const cds = [
-      { id: 1, artist: 'Mr. Nice', album: 'album1', genre: 'heavy', year: 1999 },
-      { id: 2, artist: 'Narco', album: 'album1', genre: 'heavy', year: 1939 },
-      { id: 3, artist: 'Bombasto', album: 'album1', genre: 'heavy', year: 1991 },
-      { id: 4, artist: 'Celeritas', album: 'album1', genre: 'rock', year: 2001 },
+      { id: 1, artist: 'Rammstein', album: 'Mutter', genre: 'rock', year: 2001 },
+      { id: 2, artist: 'Johnny Madsen', album: 'Le New York', genre: 'rock', year: 2011 },
+      { id: 3, artist: 'Mozart ', album: 'The Great Piano Concertos', genre: 'piano', year: 1991 },
+      { id: 4, artist: 'Metallica', album: 'Kill em All', genre: 'heavy', year: 1983 },
     ];
     return {cds};
   }
